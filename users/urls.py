dccc8126a7d6431fab74c257cd33876b69109faf
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-08-08 18:31:08 +0500

    create urls

---
+++ users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views.regitser import RegisterView
+from .views.login import LoginView
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+]
