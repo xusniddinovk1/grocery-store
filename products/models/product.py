59c9d1c3c6a3edaf8b3a0a3f81dd58f4fc708c9b
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-08-09 20:45:07 +0500

    remove field from produc model

--- products/models/product.py
+++ products/models/product.py
@@ -14,7 +14,6 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
     price = models.DecimalField(max_digits=5, decimal_places=3)
     stock = models.PositiveIntegerField(default=0)
-    is_paid = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
