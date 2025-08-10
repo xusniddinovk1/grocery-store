e230dd2714b72e5ff660e192d3e91ffd710001ff
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-08-10 21:13:00 +0500

    add flashSale model

---
+++ products/models/flash_sale.py
@@ -0,0 +1,18 @@
+from django.db import models
+from ..models import Product
+from django.utils.timezone import now
+
+
+class FlashSale(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    discount_percentage = models.PositiveIntegerField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+    def is_active(self):
+        return self.start_time <= now <= self.end_time
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['book', 'start_time', 'end_time'], name='unique_flashsale_period')
+        ]
