5bf37eb5b909177fab47fba9210259494683740a
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-08-10 13:43:19 +0500

    add order viewset

---
+++ products/views/order.py
@@ -0,0 +1,10 @@
+from rest_framework import viewsets
+from ..models import Order
+from ..serializers import OrderSerializer
+from rest_framework.permissions import IsAuthenticated
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
