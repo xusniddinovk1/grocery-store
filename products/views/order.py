ad019fe9ee843f297d3d2a5ef90a37e1d557f6f8
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-08-12 19:03:40 +0500

    add permissions

--- products/views/order.py
+++ products/views/order.py
@@ -2,13 +2,13 @@ from rest_framework import viewsets
 from ..models import Order
 from ..serializers import OrderSerializer
 from rest_framework.permissions import IsAuthenticated
-from ..permissions import IsAdminOrReadOnly
+from ..permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
-    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly, IsOwnerOrReadOnly]
 
     def get_queryset(self):
         user = self.request.user
