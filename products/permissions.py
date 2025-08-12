c8d4c9430f8d783176815b1fb41b69a8a56f3cb7
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-08-12 18:58:32 +0500

    change permissions

--- products/permissions.py
+++ products/permissions.py
@@ -1,15 +1,8 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
-class IsOwnerOrReadOnly(BasePermission):
+class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
             return True
         return request.user and request.user.is_staff
-
-
-class IsAdminOrReadOnly(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:
-            return True
-        return obj.owner == request.user
