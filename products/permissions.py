ec7064d77b4ad7db5ce9d37a94e0e90671ea1e1c
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-08-12 19:00:35 +0500

    add permissions

--- products/permissions.py
+++ products/permissions.py
@@ -6,3 +6,13 @@ class IsAdminOrReadOnly(BasePermission):
         if request.method in SAFE_METHODS:
             return True
         return request.user and request.user.is_staff
+
+
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+
+class IsOwnerOrReadOnly(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return obj.user == request.user or request.user.is_staff
+        return obj.user == request.user or request.user.is_staff
