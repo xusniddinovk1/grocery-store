0dc556b03e462c97ddb9392217d565256f44c1bd
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-08-12 18:57:05 +0500

    add permissions

--- products/views/product.py
+++ products/views/product.py
@@ -2,14 +2,14 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from ..serializers import CategorySerializer, ProductSerializer
 from ..models import Category, Product
-
+from ..permissions import IsAdminOrReadOnly
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
