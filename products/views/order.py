be9a65d0e9543fbc63246a39fd243c58250ae811
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-08-12 19:01:44 +0500

    add function for order

--- products/views/order.py
+++ products/views/order.py
@@ -9,3 +9,12 @@ class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
     permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Order.objects.all()
+        return Order.objects.filter(user=user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
