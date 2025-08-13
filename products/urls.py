866b4ca363efbdc8f56dbc947969622fc6899e2f
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-08-13 10:39:20 +0500

    update urls

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import CategoryViewSet, ProductViewSet, OrderViewSet
+from .views.flash_sale import FlashSaleViewSet
 
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet)
 router.register(r'products', ProductViewSet)
+router.register(r'flash-sales', FlashSaleViewSet)
 router.register(r'orders', OrderViewSet)
 
 urlpatterns = [
