168ef4a2216e88b66caa3e1dd7a77c8ba7281d90
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-08-18 13:38:55 +0500

    initial commit

--- config/settings.py
+++ config/settings.py
@@ -62,8 +62,12 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT'),
     }
 }
 
