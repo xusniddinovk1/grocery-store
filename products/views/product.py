18cf3fa0037b60fc153c5ed273ee567e12143441
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-08-21 14:42:46 +0500

    add override method

--- products/views/product.py
+++ products/views/product.py
@@ -1,15 +1,34 @@
+from rest_framework.response import Response
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from ..serializers import CategorySerializer, ProductSerializer
 from ..models import Category, Product
 from ..permissions import IsAdminOrReadOnly
 
+
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
 
+
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
+
+    def list(self, request, *args, **kwargs):
+        category = request.query_params.get('category', None)
+        if category:
+            self.queryset = self.queryset.filter(category=category)
+        return super().list(request, *args, **kwargs)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        related_products = Product.objects.filter(category=instance.category).exclude(id=instance.id)[:5]
+        related_serializer = ProductSerializer(related_products, many=True)
+        return Response({
+            'product': serializer.data,
+            'related_product': related_serializer.data
+        })
