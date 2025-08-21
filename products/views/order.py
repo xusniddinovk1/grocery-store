a731aa65daf01aa2423acefb370bf28c0887a928
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-08-21 14:47:22 +0500

    initial commit

--- products/views/order.py
+++ products/views/order.py
@@ -15,6 +15,3 @@ class OrderViewSet(viewsets.ModelViewSet):
         if user.is_staff:
             return Order.objects.all()
         return Order.objects.filter(user=user)
-
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
