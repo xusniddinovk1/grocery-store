000ee63128277608e5ae7b8bc97cff73b82b482c
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-08-22 15:24:13 +0500

    initial commit

--- products/views/flash_sale.py
+++ products/views/flash_sale.py
@@ -1,11 +1,17 @@
+from django_filters import rest_framework as django_filters
 from rest_framework import viewsets
 from ..models import FlashSale
 from ..permissions import IsAdminOrReadOnly
 from ..serializers import FlashSaleSerializer
 from rest_framework.permissions import IsAuthenticated
+from ..filters import FlashSaleFilter
+from rest_framework import filters
 
 
 class FlashSaleViewSet(viewsets.ModelViewSet):
     queryset = FlashSale.objects.all()
     serializer_class = FlashSaleSerializer
     permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
+
+    filter_backends = [django_filters.DjangoFilterBackend, filters.SearchFilter]
+    filterset_class = FlashSaleFilter
--- products/views/product.py
+++ products/views/product.py
@@ -1,21 +1,28 @@
 from rest_framework.response import Response
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from ..filters import ProductFilter
 from ..serializers import CategorySerializer, ProductSerializer
 from ..models import Category, Product
 from ..permissions import IsAdminOrReadOnly
+from django_filters import rest_framework as django_filters
+from rest_framework import filters
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
+    filter_backends = [django_filters.DjangoFilterBackend, filters.SearchFilter]
+    filterset_class = ProductFilter
 
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
+    filter_backends = [django_filters.DjangoFilterBackend, filters.SearchFilter]
+    filterset_class = ProductFilter
 
     def list(self, request, *args, **kwargs):
         category = request.query_params.get('category', None)
